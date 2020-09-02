7159ceabdca1323c629fdb9a8b9377cd868ddc90
Peter Pfeufer <info@ppfeufer.de>
2020-09-02 14:27:38 +0200

    settings removed, we don't need them at all

--- tnnt_templates/app_settings.py
+++
@@ -1,6 +0,0 @@
-from .utils import clean_setting
-
-# Attempt to load JS/CSS/Fonts from staticfiles when possible
-# This does not guarantee no CDN usage
-# App Developers may or may not respect this setting
-USE_TNNT_TEMPLATES = clean_setting("USE_TNNT_TEMPLATES", False)
