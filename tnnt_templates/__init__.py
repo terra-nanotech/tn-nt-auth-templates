9babfc0f860e0b52f05a1e9fc3cce22ec6abb229
Peter Pfeufer <info@ppfeufer.de>
2021-02-11 13:09:11 +0100

    that doesn't work here :-(

--- tnnt_templates/__init__.py
+++ tnnt_templates/__init__.py
@@ -1,8 +1,4 @@
-from django.conf import settings
-
 default_app_config = "tnnt_templates.apps.TnntTemplatesConfig"
 
 __version__ = "1.4.1"
-__title__ = "{entity_name} Template Overrides".format(
-    entity_name=settings.TNNT_TEMPLATE_ENTITY_NAME
-)
+__title__ = "Alliance Auth Template Overrides"
--- tnnt_templates/apps.py
+++ tnnt_templates/apps.py
@@ -1,11 +1,8 @@
 from django.apps import AppConfig
-from django.conf import settings
 from . import __version__
 
 
 class TnntTemplatesConfig(AppConfig):
     name = "tnnt_templates"
     label = "tnnt_templates"
-    verbose_name = "{entity_name} Auth Templates v{version}".format(
-        entity_name=settings.TNNT_TEMPLATE_ENTITY_TYPE, version=__version__
-    )
+    verbose_name = "Alliance Auth Templates v{version}".format(version=__version__)
