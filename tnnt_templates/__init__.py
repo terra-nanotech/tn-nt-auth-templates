aac16a182c751270b5b1289dae252bbf5cda3493
Peter Pfeufer <info@ppfeufer.de>
2022-02-20 17:02:57 +0100

    Version bumped to v2.4.0

--- tnnt_templates/__init__.py
+++ tnnt_templates/__init__.py
@@ -4,5 +4,5 @@ TN-NT Templates init
 
 default_app_config: str = "tnnt_templates.apps.TnntTemplatesConfig"
 
-__version__ = "2.3.3"
+__version__ = "2.4.0"
 __title__ = "Alliance Auth Template Overrides"
