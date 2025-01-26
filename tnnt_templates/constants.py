100ac1d7afda0efc39e623107f546f08b87e1ce2
Peter Pfeufer <info@ppfeufer.de>
2025-01-26 16:01:54 +0100

    [CHANGE] Use `django-sri` for hash calculation

---
+++ tnnt_templates/constants.py
@@ -0,0 +1,11 @@
+"""
+Constants used in this app
+"""
+
+# Standard Library
+import os
+
+TNNT_TEMPLATES_BASE_DIR = os.path.join(os.path.dirname(__file__))
+TNNT_TEMPLATES_STATIC_DIR = os.path.join(
+    TNNT_TEMPLATES_BASE_DIR, "static", "tnnt_templates"
+)
